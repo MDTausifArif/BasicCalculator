439aa7aee8cc0b67c17e5b29ebe3eed35355cc7b
MDTausifArif <142226009+MDTausifArif@users.noreply.github.com>
2024-06-25 05:28:05 +0530

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,259 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        BtnOne = New Button()
+        BtnTwo = New Button()
+        BtnThree = New Button()
+        BtnAdd = New Button()
+        BtnSubstract = New Button()
+        BtnSix = New Button()
+        BtnFive = New Button()
+        BtnFour = New Button()
+        BtnMultiply = New Button()
+        BtnNine = New Button()
+        BtnEight = New Button()
+        BtnSeven = New Button()
+        BtnDivide = New Button()
+        BtnEqual = New Button()
+        BtnClear = New Button()
+        BtnZero = New Button()
+        TextBox1 = New TextBox()
+        SuspendLayout()
+        ' 
+        ' BtnOne
+        ' 
+        BtnOne.Font = New Font("Arial Rounded MT Bold", 15.75F)
+        BtnOne.Location = New Point(37, 191)
+        BtnOne.Name = "BtnOne"
+        BtnOne.Size = New Size(65, 40)
+        BtnOne.TabIndex = 0
+        BtnOne.Text = "1"
+        BtnOne.UseVisualStyleBackColor = True
+        ' 
+        ' BtnTwo
+        ' 
+        BtnTwo.Font = New Font("Arial Rounded MT Bold", 15.75F)
+        BtnTwo.Location = New Point(108, 191)
+        BtnTwo.Name = "BtnTwo"
+        BtnTwo.Size = New Size(65, 40)
+        BtnTwo.TabIndex = 1
+        BtnTwo.Text = "2"
+        BtnTwo.UseVisualStyleBackColor = True
+        ' 
+        ' BtnThree
+        ' 
+        BtnThree.Font = New Font("Arial Rounded MT Bold", 15.75F)
+        BtnThree.Location = New Point(179, 191)
+        BtnThree.Name = "BtnThree"
+        BtnThree.Size = New Size(65, 40)
+        BtnThree.TabIndex = 2
+        BtnThree.Text = "3"
+        BtnThree.UseVisualStyleBackColor = True
+        ' 
+        ' BtnAdd
+        ' 
+        BtnAdd.Font = New Font("Arial Rounded MT Bold", 15.75F)
+        BtnAdd.Location = New Point(268, 191)
+        BtnAdd.Name = "BtnAdd"
+        BtnAdd.Size = New Size(65, 40)
+        BtnAdd.TabIndex = 3
+        BtnAdd.Text = "+"
+        BtnAdd.UseVisualStyleBackColor = True
+        ' 
+        ' BtnSubstract
+        ' 
+        BtnSubstract.Font = New Font("Arial Rounded MT Bold", 15.75F)
+        BtnSubstract.Location = New Point(268, 237)
+        BtnSubstract.Name = "BtnSubstract"
+        BtnSubstract.Size = New Size(65, 40)
+        BtnSubstract.TabIndex = 7
+        BtnSubstract.Text = "-"
+        BtnSubstract.UseVisualStyleBackColor = True
+        ' 
+        ' BtnSix
+        ' 
+        BtnSix.Font = New Font("Arial Rounded MT Bold", 15.75F)
+        BtnSix.Location = New Point(179, 237)
+        BtnSix.Name = "BtnSix"
+        BtnSix.Size = New Size(65, 40)
+        BtnSix.TabIndex = 6
+        BtnSix.Text = "6"
+        BtnSix.UseVisualStyleBackColor = True
+        ' 
+        ' BtnFive
+        ' 
+        BtnFive.Font = New Font("Arial Rounded MT Bold", 15.75F)
+        BtnFive.Location = New Point(108, 237)
+        BtnFive.Name = "BtnFive"
+        BtnFive.Size = New Size(65, 40)
+        BtnFive.TabIndex = 5
+        BtnFive.Text = "5"
+        BtnFive.UseVisualStyleBackColor = True
+        ' 
+        ' BtnFour
+        ' 
+        BtnFour.Font = New Font("Arial Rounded MT Bold", 15.75F)
+        BtnFour.Location = New Point(37, 237)
+        BtnFour.Name = "BtnFour"
+        BtnFour.Size = New Size(65, 40)
+        BtnFour.TabIndex = 4
+        BtnFour.Text = "4"
+        BtnFour.UseVisualStyleBackColor = True
+        ' 
+        ' BtnMultiply
+        ' 
+        BtnMultiply.Font = New Font("Arial Rounded MT Bold", 15.75F)
+        BtnMultiply.Location = New Point(268, 283)
+        BtnMultiply.Name = "BtnMultiply"
+        BtnMultiply.Size = New Size(65, 40)
+        BtnMultiply.TabIndex = 11
+        BtnMultiply.Text = "*"
+        BtnMultiply.UseVisualStyleBackColor = True
+        ' 
+        ' BtnNine
+        ' 
+        BtnNine.Font = New Font("Arial Rounded MT Bold", 15.75F)
+        BtnNine.Location = New Point(179, 283)
+        BtnNine.Name = "BtnNine"
+        BtnNine.Size = New Size(65, 40)
+        BtnNine.TabIndex = 10
+        BtnNine.Text = "9"
+        BtnNine.UseVisualStyleBackColor = True
+        ' 
+        ' BtnEight
+        ' 
+        BtnEight.Font = New Font("Arial Rounded MT Bold", 15.75F)
+        BtnEight.Location = New Point(108, 283)
+        BtnEight.Name = "BtnEight"
+        BtnEight.Size = New Size(65, 40)
+        BtnEight.TabIndex = 9
+        BtnEight.Text = "8"
+        BtnEight.UseVisualStyleBackColor = True
+        ' 
+        ' BtnSeven
+        ' 
+        BtnSeven.Font = New Font("Arial Rounded MT Bold", 15.75F)
+        BtnSeven.Location = New Point(37, 283)
+        BtnSeven.Name = "BtnSeven"
+        BtnSeven.Size = New Size(65, 40)
+        BtnSeven.TabIndex = 8
+        BtnSeven.Text = "7"
+        BtnSeven.UseVisualStyleBackColor = True
+        ' 
+        ' BtnDivide
+        ' 
+        BtnDivide.Font = New Font("Arial Rounded MT Bold", 15.75F)
+        BtnDivide.Location = New Point(268, 329)
+        BtnDivide.Name = "BtnDivide"
+        BtnDivide.Size = New Size(65, 40)
+        BtnDivide.TabIndex = 15
+        BtnDivide.Text = "/"
+        BtnDivide.UseVisualStyleBackColor = True
+        ' 
+        ' BtnEqual
+        ' 
+        BtnEqual.Font = New Font("Arial Rounded MT Bold", 15.75F)
+        BtnEqual.Location = New Point(179, 329)
+        BtnEqual.Name = "BtnEqual"
+        BtnEqual.Size = New Size(65, 40)
+        BtnEqual.TabIndex = 14
+        BtnEqual.Text = "="
+        BtnEqual.UseVisualStyleBackColor = True
+        ' 
+        ' BtnClear
+        ' 
+        BtnClear.Font = New Font("Arial Rounded MT Bold", 15.75F)
+        BtnClear.Location = New Point(108, 329)
+        BtnClear.Name = "BtnClear"
+        BtnClear.Size = New Size(65, 40)
+        BtnClear.TabIndex = 13
+        BtnClear.Text = "C"
+        BtnClear.UseVisualStyleBackColor = True
+        ' 
+        ' BtnZero
+        ' 
+        BtnZero.Font = New Font("Arial Rounded MT Bold", 15.75F)
+        BtnZero.Location = New Point(37, 329)
+        BtnZero.Name = "BtnZero"
+        BtnZero.Size = New Size(65, 40)
+        BtnZero.TabIndex = 12
+        BtnZero.Text = "0"
+        BtnZero.UseVisualStyleBackColor = True
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Font = New Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        TextBox1.ForeColor = SystemColors.WindowFrame
+        TextBox1.Location = New Point(38, 149)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(288, 39)
+        TextBox1.TabIndex = 16
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(377, 450)
+        Controls.Add(TextBox1)
+        Controls.Add(BtnDivide)
+        Controls.Add(BtnEqual)
+        Controls.Add(BtnClear)
+        Controls.Add(BtnZero)
+        Controls.Add(BtnMultiply)
+        Controls.Add(BtnNine)
+        Controls.Add(BtnEight)
+        Controls.Add(BtnSeven)
+        Controls.Add(BtnSubstract)
+        Controls.Add(BtnSix)
+        Controls.Add(BtnFive)
+        Controls.Add(BtnFour)
+        Controls.Add(BtnAdd)
+        Controls.Add(BtnThree)
+        Controls.Add(BtnTwo)
+        Controls.Add(BtnOne)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents BtnOne As Button
+    Friend WithEvents BtnTwo As Button
+    Friend WithEvents BtnThree As Button
+    Friend WithEvents BtnAdd As Button
+    Friend WithEvents BtnSubstract As Button
+    Friend WithEvents BtnSix As Button
+    Friend WithEvents BtnFive As Button
+    Friend WithEvents BtnFour As Button
+    Friend WithEvents BtnMultiply As Button
+    Friend WithEvents BtnNine As Button
+    Friend WithEvents BtnEight As Button
+    Friend WithEvents BtnSeven As Button
+    Friend WithEvents BtnDivide As Button
+    Friend WithEvents BtnEqual As Button
+    Friend WithEvents BtnClear As Button
+    Friend WithEvents BtnZero As Button
+    Friend WithEvents TextBox1 As TextBox
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,100 @@
+﻿Public Class Form1
+
+
+    Dim value1, value2 As Integer
+    Dim sign As String
+
+
+    Private Sub BtnOne_Click(sender As Object, e As EventArgs) Handles BtnOne.Click
+        TextBox1.Text = TextBox1.Text & 1
+
+
+    End Sub
+
+    Private Sub BtnTwo_Click(sender As Object, e As EventArgs) Handles BtnTwo.Click
+        TextBox1.Text = TextBox1.Text & 2
+
+    End Sub
+
+    Private Sub BtnThree_Click(sender As Object, e As EventArgs) Handles BtnThree.Click
+        TextBox1.Text = TextBox1.Text & 3
+    End Sub
+
+    Private Sub BtnFour_Click(sender As Object, e As EventArgs) Handles BtnFour.Click
+        TextBox1.Text = TextBox1.Text & 4
+    End Sub
+
+    Private Sub BtnFive_Click(sender As Object, e As EventArgs) Handles BtnFive.Click
+        TextBox1.Text = TextBox1.Text & 5
+    End Sub
+
+    Private Sub BtnSix_Click(sender As Object, e As EventArgs) Handles BtnSix.Click
+        TextBox1.Text = TextBox1.Text & 6
+    End Sub
+
+    Private Sub BtnSeven_Click(sender As Object, e As EventArgs) Handles BtnSeven.Click
+        TextBox1.Text = TextBox1.Text & 7
+    End Sub
+
+    Private Sub BtnEight_Click(sender As Object, e As EventArgs) Handles BtnEight.Click
+        TextBox1.Text = TextBox1.Text & 8
+    End Sub
+
+    Private Sub BtnNine_Click(sender As Object, e As EventArgs) Handles BtnNine.Click
+        TextBox1.Text = TextBox1.Text & 9
+    End Sub
+
+    Private Sub BtnZero_Click(sender As Object, e As EventArgs) Handles BtnZero.Click
+        TextBox1.Text = TextBox1.Text & 0
+    End Sub
+
+    Private Sub BtnAdd_Click(sender As Object, e As EventArgs) Handles BtnAdd.Click
+        value1 = TextBox1.Text
+        TextBox1.Clear()
+        TextBox1.Focus()
+        sign = "+"
+
+    End Sub
+
+    Private Sub BtnSubstract_Click(sender As Object, e As EventArgs) Handles BtnSubstract.Click
+        value1 = TextBox1.Text
+        TextBox1.Clear()
+        TextBox1.Focus()
+        sign = "-"
+    End Sub
+
+    Private Sub BtnMultiply_Click(sender As Object, e As EventArgs) Handles BtnMultiply.Click
+        value1 = TextBox1.Text
+        TextBox1.Clear()
+        TextBox1.Focus()
+        sign = "*"
+    End Sub
+
+    Private Sub BtnDivide_Click(sender As Object, e As EventArgs) Handles BtnDivide.Click
+        value1 = TextBox1.Text
+        TextBox1.Clear()
+        TextBox1.Focus()
+        sign = "/"
+    End Sub
+
+    Private Sub BtnEqual_Click(sender As Object, e As EventArgs) Handles BtnEqual.Click
+        value2 = TextBox1.Text
+        If sign = "+" Then
+            TextBox1.Text = value1 + value2
+        ElseIf sign = "-" Then
+            TextBox1.Text = value1 - value2
+
+        ElseIf sign = "*" Then
+            TextBox1.Text = value1 * value2
+
+        Else sign = "/"
+            TextBox1.Text = value1 / value2
+        End If
+    End Sub
+
+    Private Sub BtnClear_Click(sender As Object, e As EventArgs) Handles BtnClear.Click
+        TextBox1.Clear()
+        TextBox1.Focus()
+
+    End Sub
+End Class
